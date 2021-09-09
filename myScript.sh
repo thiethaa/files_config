e36af8206875573dfd1cffda023355fcbb6b09bb
atlroc99 <tyaniceluv89@gmail.com>
2021-09-09 09:38:00 -0400

    initial

---
+++ myScript.sh
@@ -0,0 +1,21 @@
+jfrog rt del --spec=bacnetschub.json --dry-run=true --quiet=true
+
+jfrog rt del --spec=bas/admintool.json --dry-run=true --quiet=true
+jfrog rt del --spec=bas/instanceportal.json --dry-run=true --quiet=true
+jfrog rt del --spec=bas/messenger.json --dry-run=true --quiet=true
+
+jfrog rt del --spec=ivupro/addons.json   --dry-run=true --quiet=true
+jfrog rt del --spec=ivupro/content.json  --dry-run=true --quiet=true
+jfrog rt del --spec=ivupro/drivers.json   --dry-run=true --quiet=true
+jfrog rt del --spec=ivupro/factoryapplications.json --dry-run=true --quiet=true
+jfrog rt del --spec=ivupro/sals.json   --dry-run=true --quiet=true
+jfrog rt del --spec=ivupro/server.json  --dry-run=true --quiet=true
+jfrog rt del --spec=ivupro/symbols.json   --dry-run=true --quiet=true
+
+jfrog rt del --spec=webctrl/addons.json   --dry-run=true --quiet=true
+jfrog rt del --spec=webctrl/content.json  --dry-run=true --quiet=true
+jfrog rt del --spec=webctrl/drivers.json   --dry-run=true --quiet=true
+jfrog rt del --spec=webctrl/factoryapplications.json --dry-run=true --quiet=true
+jfrog rt del --spec=webctrl/sals.json   --dry-run=true --quiet=true
+jfrog rt del --spec=webctrl/server.json  --dry-run=true --quiet=true
+jfrog rt del --spec=webctrl/symbols.json   --dry-run=true --quiet=true
